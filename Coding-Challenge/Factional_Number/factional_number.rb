c2a8cde567e78ca31b16d29c802b754f7bb7c21e
MeBs2121 <react2130@yahoo.co.jp>
2021-08-10 19:07:56 +0900

    finished factional number

---
+++ Coding-Challenge/Factional_Number/factional_number.rb
@@ -0,0 +1,28 @@
+# No.1
+def factional(number)
+  result = number
+  while number > 1
+    result = result * (number - 1)
+    number = number - 1
+  end
+  result
+end
+# puts factional(5)
+
+# No.2
+def factional_v2(number)
+  result = number
+  (1..(number - 1)).each do |item|
+    result = result * item
+  end
+  result
+end
+
+# puts factional_v2(5)
+
+# No.3
+def factional_v3(number)
+  number.downto(1).reduce(:*)
+end
+
+puts factional_v3(5)
